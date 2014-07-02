679a102e3f5f4272efbcb7998585a74f3d99828d
0xAX <testtest.89@mail.ru>
2014-07-03 00:32:53 +0600

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,68 @@
-golang-desktop-notification
+notificator
 ===========================
 
-Desktop notification with golang
+Desktop notification with golang for:
+
+  * Windows with `growlnotify`;
+  * Mac OS X with `growlnotify`;
+  * Linux with `notify-send` for gnome and `kdialog` for kde.
+
+usage
+------
+
+```go
+package main
+
+import "github.com/0xAX/notificator"
+
+func main() {
+  notificator.Push(&gnomeNotificator{"/home/user/icon.png", "title", "text"})
+}
+```
+
+All notificators:
+
+```go
+type gnomeNotificator struct {
+	IconPath string
+	Title    string
+    Text     string
+}
+
+type kdeNotificator struct {
+	IconPath string
+	Title    string
+	Text     string
+}
+
+type osxNotificator struct {
+	IconPath string
+	AppName  string
+	Title    string
+	Text     string
+}
+
+type windowsNotificator struct {
+	IconPath string
+	Title    string
+	Text     string
+}
+```
+
+todo
+-----
+
+  * Add more options for different notificators.
+
+contribution
+------------
+
+  * Fork;
+  * Make changes;
+  * Send pull request;
+  * Thank you.
+
+author
+----------
+
+[@0xAX](https://twitter.com/0xAX)
---
+++ notification.go
@@ -0,0 +1,96 @@
+package notification
+
+import "reflect"
+import "os/exec"
+
+const defaultIcon = "icon/golang.png"
+
+type Notificator struct {
+	GnomeNotificator *gnomeNotificator
+	KdeNotificator   *kdeNotificator
+	OSXNotificator   *osxNotificator
+	WindowsNotificator *windowsNotificator
+}
+
+type gnomeNotificator struct {
+	IconPath string
+	Title    string
+    Text     string
+}
+
+type kdeNotificator struct {
+	IconPath string
+	Title    string
+	Text     string
+}
+
+type osxNotificator struct {
+	IconPath string
+	AppName  string
+	Title    string
+	Text     string
+}
+
+type windowsNotificator struct {
+	IconPath string
+	Title    string
+	Text     string
+}
+
+func Push(notificator interface{}) {
+	// get notificator type
+	n := reflect.TypeOf(notificator).String()
+	// check notificator
+	if  n == "*notification.gnomeNotificator" {
+		notify := &Notificator{notificator.(*gnomeNotificator), nil, nil, nil}
+		
+		var icon string = ""
+
+		if notify.GnomeNotificator.IconPath == "" {
+			icon = defaultIcon
+		} else {
+			icon = notify.GnomeNotificator.IconPath
+		}
+
+		exec.Command("notify-send", "-i", icon, notify.GnomeNotificator.Title, notify.GnomeNotificator.Text)
+		
+	} else if n == "*notification.kdeNotificator" {
+		notify := &Notificator{nil, notificator.(*kdeNotificator), nil, nil}
+
+		var icon string = ""
+		
+		if notify.KdeNotificator.IconPath == "" {
+			icon = defaultIcon
+		} else {
+			icon = notify.KdeNotificator.IconPath
+		}
+
+		exec.Command("kdialog", "--icon", icon, "--title", notify.KdeNotificator.Title, "--passivepopup", 
+                     notify.KdeNotificator.Text)
+	} else if n == "*notification.osxNotificator" {
+		notify := &Notificator{nil, nil, notificator.(*osxNotificator), nil}
+
+		var icon string = ""
+
+		if notify.OSXNotificator.IconPath == "" {
+			icon = defaultIcon
+		} else {
+			icon = notify.OSXNotificator.IconPath
+		}
+
+		exec.Command("growlnotify", "-n", notify.OSXNotificator.AppName, "--image", icon, "-m", 
+                     notify.OSXNotificator.Title, notify.OSXNotificator.Text)
+	} else {
+		notify := &Notificator{nil, nil, nil, notificator.(*windowsNotificator)}
+	
+		var icon string
+		
+		if notify.WindowsNotificator.IconPath == "" {
+			icon = defaultIcon
+		} else {
+			icon = notify.WindowsNotificator.IconPath
+		}
+		
+		exec.Command("growlnotify", "/i:", icon, "/t:", notify.WindowsNotificator.Title, notify.WindowsNotificator.Text)
+	}
+}
