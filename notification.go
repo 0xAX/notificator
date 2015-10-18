7fcd5acc9f330d9d50889ec4e032ba17c45f8896
Bruno Fidelis <bruno.fidelis004@gmail.com>
2015-10-18 18:52:51 -0200

    added critical notification

--- notification.go
+++ notification.go
@@ -10,8 +10,14 @@ type Options struct {
 	AppName     string
 }
 
+const (
+	UR_NORMAL   =	"normal"
+	UR_CRITICAL	=	"critical"
+)
+
 type notifier interface {
 	push(title string, text string, iconPath string) *exec.Cmd
+	pushCritical(title string, text string, iconPath string) *exec.Cmd
 }
 
 type Notificator struct {
@@ -19,19 +25,19 @@ type Notificator struct {
 	defaultIcon string
 }
 
-func (n Notificator) Push(title string, text string, iconPath string) error {
-
+func (n Notificator) Push(title string, text string, iconPath string, urgency string) error {
 	icon := n.defaultIcon
 
 	if iconPath != "" {
 		icon = iconPath
 	}
 
-	err := n.notifier.push(title, text, icon).Run()
-	if err != nil {
-		return err
+	if urgency == UR_CRITICAL {
+		return n.notifier.pushCritical(title, text, icon).Run()
 	}
-	return nil
+	
+	return n.notifier.push(title, text, icon).Run()
+	
 }
 
 type osxNotificator struct {
@@ -39,7 +45,12 @@ type osxNotificator struct {
 }
 
 func (o osxNotificator) push(title string, text string, iconPath string) *exec.Cmd {
-	return exec.Command("growlnotify", "-n", o.AppName, "--image", iconPath, "-m", title, text)
+	return exec.Command("growlnotify", "-n", o.AppName, "--image", iconPath, "-m", title)
+}
+
+// Causes the notification to stick around until clicked.
+func (o osxNotificator) pushCritical(title string, text string, iconPath string) *exec.Cmd {
+	return exec.Command("notify-send", "-i", iconPath, title, text, "--sticky", "-p", "2")	
 }
 
 type linuxNotificator struct{}
@@ -48,11 +59,22 @@ func (l linuxNotificator) push(title string, text string, iconPath string) *exec
 	return exec.Command("notify-send", "-i", iconPath, title, text)
 }
 
+// Causes the notification to stick around until clicked.
+func (l linuxNotificator) pushCritical(title string, text string, iconPath string) *exec.Cmd {
+	return exec.Command("notify-send", "-i", iconPath, title, text, "-u", "critical")	
+}
+
 type windowsNotificator struct{}
 
 func (w windowsNotificator) push(title string, text string, iconPath string) *exec.Cmd {
 	return exec.Command("growlnotify", "/i:", iconPath, "/t:", title, text)
 }
+
+// Causes the notification to stick around until clicked.
+func (w windowsNotificator) pushCritical(title string, text string, iconPath string) *exec.Cmd {
+	return exec.Command("notify-send", "-i", iconPath, title, text, "/s", "true", "/p", "2")	
+}
+
 
 func New(o Options) *Notificator {
 
